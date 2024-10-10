c8a3615b61995522d566371cd02f0629e7df0a40
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-10 07:53:42 +0000

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Contain to_kv method"""
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with a string and the square of the int/float."""
+    return (k, float(v ** 2))
