87179573f889b288d014266ad0195379bb547a71
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-15 21:30:21 +0000

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Contains async_comprehension method"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asynchronously collects random float values.
+
+    This function utilizes the async_generator to gather random float values
+    into a list. It runs the generator asynchronously and collects the results
+    using an asynchronous list comprehension.
+
+    Returns:
+        List[float]: A list of random float values yielded by the async_generator.
+    """
+    return [_ async for _ in async_generator()]
